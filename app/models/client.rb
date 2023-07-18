ed28e79c0068e9d9939409a62fc997b176c3ec6d
yauyouyou <iwaiwa1115@gmail.com>
2023-07-18 15:53:14 +0900

    バリデーション仮設定

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,24 @@
 class Client < ApplicationRecord
+  validates :client_firstname, presence: true
+  validates :client_lastname, presence: true
+  validates :client_firstname_kana, presence: true
+  validates :client_lastname_kana, presence: true
+  validates :birthday, presence: true
+  validates :gender_id, presence: true, exclusion: { in: [1] }
+  validates :post_code, presence: true
+  validates :prefecture_id, presence: true, exclusion: { in: [1] }
+  validates :municipality, presence: true
+  validates :address, presence: true
+  # validates :building_name
+  # validates :phone_number
+  # validates :mobile_number
+  # validates :company_name
+  # validates :project
+  # validates :note
+
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :gender
 end
