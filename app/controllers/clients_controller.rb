818be03b6544a74791f4df3f794eb7b0e4f4fcb3
yauyouyou <iwaiwa1115@gmail.com>
2023-07-18 15:28:16 +0900

    createアクション定義

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,16 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+
+  def create
+    Client.create(client_params)
+    redirect_to '/'
+  end
+
+  
+  private
+  def client_params
+    params.require(:client).permit(:client_firstname, :client_lastname, :client_firstname_kana, :client_lastname_kana, :birthday, :gender_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :mobile_number, :company_name, :project, :note)
+  end
+  
 end
