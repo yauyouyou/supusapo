1eefde3846a66e494fc80314e99cde4f05fc1c48
yauyouyou <iwaiwa1115@gmail.com>
2023-07-18 20:01:38 +0900

    モデル作成に伴いマイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_050910) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_094827) do
+  create_table "client_details", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "clients", charset: "utf8", force: :cascade do |t|
     t.string "client_firstname", null: false
     t.string "client_lastname", null: false
