e51afa5f04f2b922312ec63a633201c823163183
yauyouyou <iwaiwa1115@gmail.com>
2023-07-19 17:18:49 +0900

    追加情報のnewアクション定義

--- app/controllers/client_details_controller.rb
+++ app/controllers/client_details_controller.rb
@@ -6,25 +6,6 @@ class ClientDetailsController < ApplicationController
   end
 
   def new
-    @client = Client.find(params[:client_id])
     @client_detail = ClientDetail.new
-  end
-
-  def create
-    @client = Client.find(params[:client_id])
-    @client_detail = @client.client_details.build(client_detail_params)
-    @client_detail.fields.build if @client_detail.fields.empty?
-  
-    if @client_detail.save
-      redirect_to client_client_details_path(@client), notice: "追加情報が保存されました。"
-    else
-      render :index
-    end
-  end
-
-  private
-
-  def client_detail_params
-    params.require(:client_detail).permit(fields_attributes: [:title, :content])
   end
 end
--- app/models/client_detail.rb
+++ app/models/client_detail.rb
@@ -1,5 +1,3 @@
 class ClientDetail < ApplicationRecord
   belongs_to :client
-  has_many :fields
-  accepts_nested_attributes_for :fields
 end
