b3a4090f0aa40b5f0c29db00f33ac2ac4b2ce217
yauyouyou <iwaiwa1115@gmail.com>
2023-07-19 17:44:47 +0900

    createアクション定義

--- app/controllers/client_details_controller.rb
+++ app/controllers/client_details_controller.rb
@@ -9,4 +9,21 @@ class ClientDetailsController < ApplicationController
     @client = Client.find(params[:client_id])
     @client_detail = ClientDetail.new
   end
+
+  def create
+    @client = Client.find(params[:client_id])
+    @client_detail = @client.client_details.build(client_detail_params)
+
+    if @client_detail.save
+      redirect_to client_client_details_path(@client)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def client_detail_params
+    params.require(:client_detail).permit(:title, :content)
+  end
 end
