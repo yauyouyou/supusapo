9083c48b7c0b0d046c32a3fb798a0ed161821356
yauyouyou <iwaiwa1115@gmail.com>
2023-07-20 16:48:48 +0900

    usersテーブル姓名追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_072823) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_074758) do
   create_table "client_details", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_072823) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
